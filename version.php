c52b12f77a6cb469bda0d35785879b596c53a3ca
Stefan-Alexander Scholz <hello@bdecent.de>
2023-03-08 20:38:11 +0100

    Update version.php
    changed supported and required version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pulse';
-$plugin->version = 2022050900;
-$plugin->requires = 2022041900; // Requires Moodle 4.0.
+$plugin->version = 2023030800;
+$plugin->requires = 2020061500; // Requires Moodle 3.90.
 $plugin->release = 'v1.2';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->supported = [40, 401];
+$plugin->supported = [39, 401];
