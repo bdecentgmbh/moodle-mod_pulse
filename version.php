b3e29c335abcdd5c57ec551641ef7120bd1d7677
Stefan-Alexander Scholz <hello@bdecent.de>
2023-06-13 09:55:15 +0200

    Update version.php
    Bump version and set maturity status

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_pulse';
 $plugin->version = 2023051800;
 $plugin->requires = 2020061500; // Requires Moodle 3.90.
-$plugin->release = 'v1.2';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->release = 'v1.3';
+$plugin->maturity = MATURITY_RC;
 $plugin->supported = [39, 401];
