407582ba03a82325734fbc100f8e598769b191df
prasanna-lmsace <prasanna_t@lmsace.com>
2023-11-26 13:19:10 +0530

    Improved invitation query to send notification based on limit

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pulse';
-$plugin->version = 2023051800;
+$plugin->version = 2023051801;
 $plugin->requires = 2020061500; // Requires Moodle 3.90.
 $plugin->release = 'v1.3';
 $plugin->maturity = MATURITY_RC;
