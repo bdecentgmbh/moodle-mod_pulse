e1758a225c57ddafb6587b2563f61b1fc1414397
Vithusha Kethiri <vithushakethiri@catalyst-au.net>
2024-03-08 14:10:45 +1100

    Update lang strings

--- actions/notification/lang/en/pulseaction_notification.php
+++ actions/notification/lang/en/pulseaction_notification.php
@@ -149,3 +149,6 @@ $string['automationinstance'] = 'Automation instance';
 $string['automationtemplate'] = 'Automation template';
 
 $string['privacy:metadata:pulseaction_notification'] = 'Information about the user to send the notification';
+$string['privacy:metadata:pulseaction_notification_sch:userid'] = 'The ID of the user is tracked.';
+$string['privacy:metadata:pulseaction_notification_sch:status'] = 'The status of the user is tracked.';
+$string['privacy:metadata:pulseaction_notification_sch:timecreated'] = 'The created time of the user is tracked.';
