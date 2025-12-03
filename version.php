0b4513896505b990337bb48094abea79be3b8ea8
vasanthlmsace <vasanthr.lmsace@gmail.com>
2025-12-03 11:38:57 +0530

    Pulse support for Moodle-5.1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pulse';
-$plugin->version = 2025110100;
+$plugin->version = 2025120300;
 $plugin->requires = 2022112800; // Requires Moodle 4.1.
-$plugin->release = 'v2.3';
+$plugin->release = 'v2.5';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [401, 500];
